2e83de783a9f8b0d8dcc650a353ba708a1785b08
Yurii M <ispoyar@yandex.ru>
2024-01-29 22:03:07 +0300

    complete

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/Yurii-M76/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
+const addCardButton = document.querySelector('.profile__add-button');
+const formNewCard = document.querySelector('.popup_type_new-card');
+const closeFormNewCardButton = formNewCard.querySelector('.popup__close');
 
-// @todo: DOM узлы
+function cardCreate(name, link) {
+  const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardItem.querySelector('.card__title').textContent = name;
+  cardItem.querySelector('.card__image').src = link;
 
-// @todo: Функция создания карточки
+  cardItem.querySelector('.card__delete-button').addEventListener('click', () => {
+    cardDelete(cardItem);
+  });
 
-// @todo: Функция удаления карточки
+  cardItem.querySelector('.card__like-button').addEventListener('click', evt => {
+    evt.target.classList.toggle('card__like-button_is-active');
+  });
 
-// @todo: Вывести карточки на страницу
+  cardList.append(cardItem);
+}
+
+function cardDelete(id) {
+  id.remove();
+}
+
+function popupToggler() {
+  formNewCard.classList.toggle('popup_is-opened');
+}
+
+addCardButton.addEventListener('click', popupToggler);
+closeFormNewCardButton.addEventListener('click', popupToggler);
+
+initialCards.forEach(item => {
+  cardCreate(item.name, item.link);
+});
