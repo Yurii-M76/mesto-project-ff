c3123adee563ef256610bf6a55715162db7c95bf
Yurii M <ispoyar@yandex.ru>
2024-01-31 19:55:08 +0300

    Правки после ревью №1

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,26 @@
-const cardTemplate = document.querySelector('#card-template').content;
 const cardList = document.querySelector('.places__list');
-const addCardButton = document.querySelector('.profile__add-button');
-const formNewCard = document.querySelector('.popup_type_new-card');
-const closeFormNewCardButton = formNewCard.querySelector('.popup__close');
+const card = document.querySelector('#card-template').content;
+const cardImage = card.querySelector(".card__image");
+const cardName = card.querySelector(".card__title");
 
-function cardCreate(name, link) {
-  const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
-  cardItem.querySelector('.card__title').textContent = name;
-  cardItem.querySelector('.card__image').src = link;
+function getCard() {
+  const cardElement = card.querySelector('.card').cloneNode(true);
+  return cardElement;
+}
 
-  cardItem.querySelector('.card__delete-button').addEventListener('click', () => {
-    cardDelete(cardItem);
+function createCard(name, link) {
+  cardName.textContent = name;
+  cardImage.src = link;
+  cardImage.alt = name;
+  const cardItem = getCard();
+  cardItem.querySelector(".card__delete-button").addEventListener('click', () => {
+    deleteCard(cardItem);
   });
-
-  cardItem.querySelector('.card__like-button').addEventListener('click', evt => {
-    evt.target.classList.toggle('card__like-button_is-active');
-  });
-
   cardList.append(cardItem);
 }
 
-function cardDelete(id) {
-  id.remove();
+function deleteCard(item) {
+  item.remove();
 }
 
-function popupToggler() {
-  formNewCard.classList.toggle('popup_is-opened');
-}
-
-addCardButton.addEventListener('click', popupToggler);
-closeFormNewCardButton.addEventListener('click', popupToggler);
-
-initialCards.forEach(item => {
-  cardCreate(item.name, item.link);
-});
+initialCards.forEach(item => createCard(item.name, item.link));
